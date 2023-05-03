c795dbc6df0569a600f44713660f2f6152e6b135
Aditya <adityasinghboss1234@gmail.com>
2023-05-04 00:49:16 +0530

    Authentication done
    Signed-off-by: Aditya <adityasinghboss1234@gmail.com>

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import {
   Body,
   Post,
   Controller,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 // import { } from "@nestjs/common/decorators";
 import { AuthService } from './auth.service';
@@ -16,6 +18,7 @@ export class AuthController {
     return this.authService.signup(dto);
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('signin')
   signin(@Body() dto: AuthDto) {
     return this.authService.signin(dto);

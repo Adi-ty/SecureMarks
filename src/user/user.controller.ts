dd5ea8fa2d72857728b4497ef4e2bd5f2430ab2c
Aditya <adityasinghboss1234@gmail.com>
2023-05-04 00:44:25 +0530

    decorator and gaurd implemented
    Signed-off-by: Aditya <adityasinghboss1234@gmail.com>

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,17 @@
 import {
   Controller,
   Get,
-  Req,
   UseGuards,
 } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+import { User } from '@prisma/client';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User) {
+    return user;
   }
 }

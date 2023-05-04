a613d45f524139561d88c916585455c71281033a
Aditya <adityasinghboss1234@gmail.com>
2023-05-04 20:50:15 +0530

    Authentication Api e2e
    Signed-off-by: Aditya <adityasinghboss1234@gmail.com>

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,7 +4,9 @@ import {
   INestApplication,
   ValidationPipe,
 } from '@nestjs/common';
+import * as pactum from 'pactum';
 import { PrismaService } from '../src/prisma/prisma.service';
+import { AuthDto } from '../src/auth/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -22,9 +24,13 @@ describe('App e2e', () => {
       }),
     );
     await app.init();
+    await app.listen(3000);
 
     prisma = app.get(PrismaService);
     await prisma.cleanDb();
+    pactum.request.setBaseUrl(
+      'http://localhost:3000',
+    );
   });
 
   afterAll(() => {
@@ -32,12 +38,82 @@ describe('App e2e', () => {
   });
 
   describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'test@email.com',
+      password: '1234',
+    };
     describe('Signup', () => {
-      it.todo('should signup');
+      it('should throw exception if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw if password empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw if no body', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .expectStatus(400);
+      });
+
+      it('should signup', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
     });
 
     describe('Signin', () => {
-      it.todo('should signin');
+      it('should throw exception if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw if password empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw if no body', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .expectStatus(400);
+      });
+
+      it('should signin', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200);
+      });
     });
   });
 
